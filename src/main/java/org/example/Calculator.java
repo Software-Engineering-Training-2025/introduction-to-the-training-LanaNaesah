ada2fdb0c1bfae69cecbc2c42714c57e0e7c4f2a
LanaNaesah <lananaesah@gmail.com>
2025-08-25 22:05:39 +0300

    Update Calculator.java

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -7,19 +7,18 @@ public class Calculator {
     }
 
     public double calculate(double a, double b, Operator op) {
-        if (op == Operator.ADD) {
-            return a + b;
-        } else if (op == Operator.SUBTRACT) {
-            return a - b;
-        } else if (op == Operator.MULTIPLY) {
-            return a * b;
-        } else if (op == Operator.DIVIDE) {
-            if (b==0) {
-                throw new ArithmaticException("Division by zero is not allowed");
-            }
-            return a / b;
-         else {
-            throw new IllegalArgumentException("Unknown operator");
+    if (op == Operator.ADD) {
+        return a + b;
+    } else if (op == Operator.SUBTRACT) {
+        return a - b;
+    } else if (op == Operator.MULTIPLY) {
+        return a * b;
+    } else if (op == Operator.DIVIDE) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed");
         }
+        return a / b;
+    } else {
+        throw new IllegalArgumentException("Unknown operator");
     }
 }
